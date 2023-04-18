266f70b139537d67f28a5b57732ea672c38a34ac
Cy-Bert <cyril.humbert.70@gmail.com>
2023-04-18 08:57:37 +0200

    🎨 style(allerplusloin.html, footer.html, index.html, script.js) : corriger les chemins d'accès des images et des fichiers HTML Les chemins d'accès des images et des fichiers HTML ont été corrigés pour qu'ils pointent vers les bons emplacements. Cela permet de s'assurer que les images et les fichiers HTML sont correctement affichés sur le site web. De plus, le script.js a été modifié pour charger dynamiquement le contenu des fichiers HTML dans les sections correspondantes.

--- src/script.js
+++ src/script.js
@@ -2,6 +2,9 @@
 // Import des elements HTML
 
 let listChildren = body.children;
+let listMainChildren = document.getElementById('main').children;
+
+console.log(listMainChildren)
 
 for (let i = 0; i < listChildren.length; i++) {
     
@@ -14,6 +17,20 @@ for (let i = 0; i < listChildren.length; i++) {
       }
     };
     xhr.open("GET", listChildren[i].id + ".html", true);
+    xhr.send();
+  }
+}
+for (let i = 0; i < listMainChildren.length; i++) {
+    
+  if (listMainChildren[i].id != "") {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        document.getElementById(listMainChildren[i].id).innerHTML =
+          xhr.responseText;
+      }
+    };
+    xhr.open("GET", listMainChildren[i].id + ".html", true);
     xhr.send();
   }
 }
