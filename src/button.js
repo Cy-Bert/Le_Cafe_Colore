c06e047eb4b77e804d36ad10ef781cae3f7ecd9d
Nara64 <127386715+Nara64@users.noreply.github.com>
2023-04-19 15:12:27 +0200

    factorisation

--- src/button.js
+++ src/button.js
@@ -1,59 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let button = document.querySelector('#unicornRain');
-  let container = document.querySelector('#unicornContainer');
+const button = document.querySelector('#unicornRain');
+const container = document.querySelector('#unicornContainer');
 
-  function launchRain() {
-    for (let i = 0; i < 200; i++) {
-      let unicorn = document.createElement('img');
-      unicorn.setAttribute('src', 'https://api.iconify.design/fluent-emoji-flat:unicorn.svg?color=%23ffd700');
-      unicorn.classList.add('w-12', 'h-auto', 'animate-unicorn');
-      unicorn.style.position = 'relative';
-      unicorn.style.left = Math.random() * (window.innerWidth - 100) + 'px';
-      // unicorn.style.top = '80px';
-      // unicorn.style.animation = 'unicorn 1s ease forwards';
-      unicorn.style.animationDelay = Math.random() * 3 + 's';
-      container.appendChild(unicorn);
+function launchRain() {
+  for (let i = 0; i < 200; i++) {
+    const icons = ['https://api.iconify.design/fluent-emoji-flat:unicorn.svg?color=%23ffd700', 'https://api.iconify.design/fluent-emoji-flat:rainbow.svg?color=%23ffd700', 'https://api.iconify.design/iconoir:bright-star.svg?color=%23ffd700', 'https://api.iconify.design/fluent-emoji:kissing-cat.svg?color=%23ffd700'];
+    
+    const iconIndex = Math.floor(Math.random() * icons.length);
+    const icon = icons[iconIndex];
 
-      let rainbow = document.createElement('img');
-      rainbow.setAttribute('src', 'https://api.iconify.design/fluent-emoji-flat:rainbow.svg?color=%23ffd700');
-      rainbow.classList.add('w-12', 'h-auto', 'animate-unicorn');
-      rainbow.style.position = 'relative';
-      rainbow.style.left = Math.random() * (window.innerWidth - 100) + 'px';
-      // rainbow.style.top = '100px';
-      // rainbow.style.animation = 'rainbow 1s ease forwards';
-      rainbow.style.animationDelay = Math.random() * 3 + 's';
-      container.appendChild(rainbow);
+    const img = document.createElement('img');
+    img.setAttribute('src', icon);
+    img.classList.add('w-12', 'h-auto', 'animate-unicorn');
+    img.style.position = 'absolute';
+    img.style.left = Math.random() * (window.innerWidth - 100) + 'px';
+    img.style.top = Math.random() * (window.innerHeight - 100) + 'px';
+    img.style.animation = 'unicorn 1s ease forwards';
+    img.style.animationDelay = Math.random() * 3 + 's';
 
-      let star = document.createElement('img');
-      star.setAttribute('src','https://api.iconify.design/iconoir:bright-star.svg?color=%23ffd700');
-      star.classList.add('w-16', 'h-auto','animate-unicorn');
-      star.style.position = 'relative';
-      star.style.left = Math.random() * (window.innerWidth - 100) + 'px';
-      // star.style.top = '50px';
-      // star.style.animation = 'star 5s ease forwards';
-      star.style.animationDelay = Math.random() * 3 + 's';
-      container.appendChild(star);
-
-      let cat = document.createElement('img');
-      cat.setAttribute('src','https://api.iconify.design/fluent-emoji:kissing-cat.svg?color=%23ffd700');
-      cat.classList.add('w-12', 'h-auto','animate-unicorn');
-      cat.style.position = 'relative';
-      cat.style.left = Math.random() * (window.innerWidth - 100) + 'px';
-      // star.style.top = '50px';
-      // star.style.animation = 'star 5s ease forwards';
-      cat.style.animationDelay = Math.random() * 3 + 's';
-      container.appendChild(cat);
-
-    }
+    container.appendChild(img);
   }
+}
 
-  button.addEventListener('click', function() {
-    launchRain();
-  });
+button.addEventListener('click', launchRain);
 
-  document.addEventListener('animationend', function(event) {
-    if (event.animationName === 'unicorn' || event.animationName === 'rainbow' || event.animationName === 'star' || event.animationName === 'cat') {
-      event.target.remove();
-    }
-  });
+container.addEventListener('animationend', function(event) {
+  event.target.remove();
 });
