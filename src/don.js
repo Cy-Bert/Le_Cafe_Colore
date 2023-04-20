ec1c383a869c3dce6cf709f9b0f04d79480a18ff
Cy-Bert <cyril.humbert.70@gmail.com>
2023-04-20 11:32:59 +0200

    🎨 style(index.html) : renommer les identifiants des éléments de formulaire de don pour plus de clarté 🎨 style(don.js) : renommer les variables pour correspondre aux nouveaux identifiants des éléments de formulaire de don 🐛 corriger(don.js) : ajouter la fonctionnalité de fermeture de la fenêtre contextuelle de don en cliquant sur la croix en haut à droite de la fenêtre.

--- src/don.js
+++ src/don.js
@@ -1,13 +1,18 @@
 const donBtn = document.getElementById("btn_don");
 const popupDon = document.getElementById("don-popup");
-const modalDon = document.getElementById("modalDon");
+const closePopupDon = document.querySelector("#closeFormDon");
+const closeCrossD = document.querySelector("#closeCrossDon")
 
 donBtn.addEventListener("click", function () {
     popupDon.classList.toggle("hidden");
     popupDon.classList.toggle("flex");
 });
 
-modalDon.addEventListener("click", function (event) {
-    popupDon.classList.toggle("hidden");
-    popupDon.classList.toggle("flex");
-});
+closePopupDon.addEventListener("click", function () {
+    popupDon.classList.add("hidden") ; 
+  });
+  
+  closeCrossDon.addEventListener("click", function() {
+      popupDon.classList.add("hidden");
+  });
+  
