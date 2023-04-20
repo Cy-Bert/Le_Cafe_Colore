90740c822ef9822659b5b3ab0d6b302738b0f727
Cy-Bert <cyril.humbert.70@gmail.com>
2023-04-20 09:14:08 +0200

    mise en preprod

--- src/script.js
+++ src/script.js
@@ -1,28 +1,27 @@
 // Import des elements HTML
 
-let listChildren = body.children;
-let listMainChildren = document.querySelector('#main').children;
+// let listChildren = body.children;
+// let listMainChildren = document.querySelector('#main').children;
 
-//Définir ma fonction qui se nomme fetchHTML
-function fetchHtml(list){
-  for (let i = 0; i < list.length; i++) {
+// function fetchHtml(list){
+//   for (let i = 0; i < list.length; i++) {
     
-    if (list[i].id != "") {
-        const xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-          document.querySelector("#"+list[i].id).innerHTML =
-            xhr.responseText;
-        }
-      };
-      xhr.open("GET", list[i].id + ".html", true);
-      xhr.send();
-    }
-  }
-}
+//     if (list[i].id != "") {
+//         const xhr = new XMLHttpRequest();
+//       xhr.onreadystatechange = function () {
+//         if (this.readyState === 4 && this.status === 200) {
+//           document.querySelector("#"+list[i].id).innerHTML =
+//             xhr.responseText;
+//         }
+//       };
+//       xhr.open("GET", list[i].id + ".html", true);
+//       xhr.send();
+//     }
+//   }
+// }
 
-//Appeler mes fonctions
-fetchHtml(listChildren)
-fetchHtml(listMainChildren)
+
+// fetchHtml(listChildren)
+// fetchHtml(listMainChildren)
 
 
